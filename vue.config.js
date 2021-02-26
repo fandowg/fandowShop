af19264ebe1a1e78a5be0a1b1270f9919c0c36c8
fandowg <fandow@msn.com>
2021-02-26 11:27:38 +0800

    修改設定

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  // publicPath: './',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/fandowShop/'
-    : '/',
-  css: {
-    extract: false
-  }
+    : '/'
+  // css: {
+  //   extract: false
+  // }
 }
